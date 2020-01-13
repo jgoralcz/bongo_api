eedd7ca169fa3741bece8baaf0206a330aa62a3d
jgoralcz <jgoralcz@asu.edu>
2020-01-12 18:24:18 -0700

    fix woops

---
+++ src/routes/fix.js
@@ -0,0 +1,21 @@
+const route = require('express-promise-router')();
+
+const {
+  getWaifuById, getRandomBadWaifu, updateWaifu,
+} = require('../db/waifu_schema/waifu/waifu');
+
+route.get('/', async (req, res) => {
+  const query = await getRandomBadWaifu('Cupcake', 'Emojis');
+  for (let i = 0; i < query.length; i += 1) {
+    const bad = query[i];
+    if (bad && bad.id) {
+      const waifu = await getWaifuById(bad.id);
+      if (waifu && waifu[0]) {
+        console.log(waifu);
+        await updateWaifu(waifu[0]);
+      }
+    }
+  }
+});
+
+module.exports = route;
